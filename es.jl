9f7d6e3a8a715fa442622d01f0de7a7b8090baf2
Gabriel Levine <gabriel.levine@utc.com>
2018-08-30 22:28:22 -0400

    initial commit

---
+++ es.jl
@@ -0,0 +1,17 @@
+"""Sieve of Eratosthenes function docstring"""
+function es(n::Int) # accepts one integer argument
+    isprime = trues(n) # n-element vector of true-s
+    isprime[1] = false # 1 is not a prime
+    for i in 2:isqrt(n) # loop odd integers less or equal than sqrt(n)
+        if isprime[i] # conditional evaluation
+            for j in i^2:i:n # sequence with step i
+                isprime[j] = false
+            end
+        end
+    end
+    return filter(x -> isprime[x], 1:n) # filter using anonymous function
+end
+
+println(es(100)) # print all primes less or equal than 100
+
+@time length(es(10^6)) # check function execution time and memory usage
